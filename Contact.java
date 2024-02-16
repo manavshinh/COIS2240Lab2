da8dab8bf9a3f23b0d3719bd8624c6ed2e1c525c
Manav <manavshinh@trentu.ca>
2024-02-16 16:13:57 -0500

    Commit 1

--- Contact.java
+++ Contact.java
@@ -27,5 +27,30 @@ class Contact {
     }
 
     // Getters and Setters (if necessary)
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String  name){
+        this.name = name;
+
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber= phoneNumber;
+    }
     // ...
 }
--- ContactManager.java
+++ ContactManager.java
@@ -14,17 +14,27 @@ class ContactManager {
 
     // Method to add a new Contact
     public void addContact(Contact contact) {
-        // Your code here
+        contacts.add(contact);
     }
 
     // Method to remove a Contact by name
     public void removeContact(String name) {
-        // Your code here
+     for(Contact contact: contacts){
+        if (contact.getName().equals(name)) {
+            contacts.remove(contact);
+            System.out.println("Contact"+ name + "removed successfully.");
+            return;
+        }
+     }   
+     System.out.println("Contact"+ name + "not found.");
     }
 
     // Method to list all Contacts
     public void listContacts() {
-        // Your code here
+        System.out.println("List of Contacts:");
+        for (Contact contact : contacts) {
+            System.out.println("Name: " + contact.getName() + ", Phone Number:" + contact.getPhoneNumber());
+        }
     }
 
 
